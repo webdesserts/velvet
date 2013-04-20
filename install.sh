41eac9e90d30a2bebe32cbbdf6dd4c88c2bbf339
Michael Mullins <michael@WebDesserts.(none)>
2013-04-20 04:09:01 -0500

    linking rather than copying repo

--- install.sh
+++ install.sh
@@ -1,29 +1,35 @@
 #! /bin/bash
 
 os=$OSTYPE
+pwd=`pwd`
+
 
 echo $os
 if [[ "$os" = linux* ]]; then
   echo "linux system detected"
-
-  # move ~/.vim directory
-  if [[ -d $HOME/.vim ]]; then
-    echo "~/.vim folder found. cleaning."
-    rm -rf $HOME/.vim
-  fi
-  echo "creating ~/.vim folder"
-  cp -r ./ $HOME/.vim
-
-  # move .vimrc
-  if [[ -e $HOME/.vimrc ]]; then
-    echo "removing current ~/.vimrc"
-    rm $HOME/.vimrc
-  fi
-  echo "creating new ~/.vimrc"
-  cp local/vimrc $HOME/.vimrc
+  vimfiles=$HOME/.vim
+  vimrc=$HOME/.vimrc
 else
   echo "assuming windows"
+  vimfiles=$HOME/vimfiles
+  vimrc=$HOME/_vimrc
 fi
+
+  # move ~/.vim directory
+  if [[ -d $vimfiles ]]; then
+    echo "vim folder found. cleaning."
+    rm -rf $vimfiles
+  fi
+  echo "creating vim link"
+  ln -s $pwd $vimfiles
+
+  # move .vimrc
+  if [[ -f $vimrc ]]; then
+    echo "removing current vimrc"
+    rm $vimrc
+  fi
+  echo "creating vimrc ln"
+  ln local/vimrc $vimrc
 
 echo "Installing Packages"
 vim +BundleInstall +qall
