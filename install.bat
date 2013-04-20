ad5dee347e607aef72953ec795c9d696e3cabf57
Michael Mullins <webdesserts@gmail.com>
2013-04-20 17:34:11 -0500

    REALLY basic batch file for windows

---
+++ install.bat
@@ -0,0 +1,19 @@
+@echo off
+set home=%USERPROFILE%
+set localrc=%cd%\local\vimrc
+set vimrc=%home%\_vimrc
+set vim=%home%\_vimfiles
+
+if exist "%vimrc%" (
+  echo vimrc already exists
+) else (
+  mklink %vimrc% %localrc%
+)
+
+if exist %vim% (
+  echo vimfiles already exist
+) else (
+ mklink %vim% %cd%
+)
+
+:END
