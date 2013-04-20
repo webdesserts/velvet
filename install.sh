4d21ba646a284bb6ddbf79159df6cbc863e94fc5
Michael Mullins <webdesserts@gmail.com>
2013-04-20 01:43:01 -0500

    adding basic install script for linux setup

---
+++ install.sh
@@ -0,0 +1,31 @@
+#! /bin/bash
+
+echo "pulling submodules"
+
+git submodule init
+git submodule update
+
+os=$OSTYPE
+
+echo $os
+if [[ "$os" = linux* ]]; then
+  echo "linux system detected"
+
+  # move ~/.vim directory
+  if [[ -d $HOME/.vim ]]; then
+    echo "~/.vim folder found. cleaning."
+    rm -rf $HOME/.vim
+  fi
+  echo "creating ~/.vim folder"
+  cp -r ./ $HOME/.vim
+
+  # move .vimrc
+  if [[ -e $HOME/.vimrc ]]; then
+    echo "removing current ~/.vimrc"
+    rm $HOME/.vimrc
+  fi
+  echo "creating new ~/.vimrc"
+  cp local/vimrc $HOME/.vimrc
+else
+  echo "assuming windows"
+fi
